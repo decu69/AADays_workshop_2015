WhiteRookPL/test-pyramid-workshop-qe-2014

[TAG] BDD_2 task.

application/todo-list/RUN_BDD.bat
@@ -1,1 +1,1 @@
-bundle exec cucumber features -c -t @bdd1
+bundle exec cucumber features -c -t @bdd2

decu69/AADays_workshop_2015

application/todo-list/RUN_BDD.bat
@@ -1,1 +1,1 @@
-bundle exec cucumber features -c -t @bdd1
+bundle exec cucumber features -c -t @bdd3